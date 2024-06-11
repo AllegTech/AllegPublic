7af3cb8fc0d53b8a70c119909d0f7c5da0c9cca2
Nick Noonan <nsnoonan@gmail.com>
2024-06-11 16:51:09 -0500

    Update Get-NoPasswordRequired.ps1

--- Security/Get-NoPasswordRequired.ps1
+++ Security/Get-NoPasswordRequired.ps1
@@ -11,19 +11,30 @@
         * Script created.
 #>
 
-# domain trust accounts need to have NoPasswordSet, so grab each trust
-$trustedDomains = @()
-$trusts = Get-AdTrust -filter * -properties FlatName
-foreach ($trust in $trusts) {
-    $trustedDomains += "$($trust.FlatName)"
-}
+# only run on the PDC to avoid tickets for each DC
+if (((Get-ADDomain | Select-Object PDCEmulator).PDCEmulator -replace "\..*$","") -eq $env:computername) {
 
-# get all users with this set
-$nonCompliantUsers = @()
-$nonCompliantUsers += Get-Aduser -filter {PasswordNotRequired -eq $true} -properties whenChanged
-# remove the accounts tied to domain trust from the list
-foreach ($domain in $trustedDomains) {
-    $nonCompliantUsers = $nonCompliantUsers | where-object {$_.name -ne "$($domain)$"}
-}
+    # grab each trust so they can be excluded later
+    $trustedDomains = @()
+    $trusts = Get-AdTrust -filter * -properties FlatName
+    foreach ($trust in $trusts) {
+        $trustedDomains += "$($trust.FlatName)"
+    }
 
-return $nonCompliantUsers
+    # get all users with PasswordNotRequired set
+    $nonCompliantUsers = @()
+    $nonCompliantUsers += Get-Aduser -filter {PasswordNotRequired -eq $true} -properties whenChanged
+    # remove the accounts tied to domain trust from the list
+    foreach ($domain in $trustedDomains) {
+        $nonCompliantUsers = $nonCompliantUsers | where-object {$_.name -ne "$($domain)$"}
+    }
+
+    # return the list
+    if ($nonCompliantUsers) {
+        return $nonCompliantUsers
+    } else {
+        return $null
+    }
+} else {
+    return $null
+}
