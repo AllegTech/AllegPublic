fcc94c62fee42f476d8c106b23d9089d43e3aa02
Nick Noonan <nsnoonan@gmail.com>
2024-06-12 07:46:45 -0500

    Get-NoPasswordRequiredAccounts
    Updated the name and added some clarifying notes.

--- Security/Get-NoPasswordRequiredAccounts.ps1
+++ Security/Get-NoPasswordRequiredAccounts.ps1
@@ -1,7 +1,6 @@
 <#
 .SYNOPSIS
     Returns a list of domain users with PasswordNotRequired set.
-    Skips accounts used for domain trusts, as those are required to have PaswordNotRequired set.
 
 .NOTES
     CREATE DATE:    2024-06-11
@@ -14,7 +13,8 @@
 # only run on the PDC to avoid tickets for each DC
 if (((Get-ADDomain | Select-Object PDCEmulator).PDCEmulator -replace "\..*$","") -eq $env:computername) {
 
-    # grab each trust so they can be excluded later
+    # domain trust account require the PasswordNotRequired attribute
+    # grab each domain trust so they can be excluded later
     $trustedDomains = @()
     $trusts = Get-AdTrust -filter * -properties FlatName
     foreach ($trust in $trusts) {
@@ -24,17 +24,17 @@ if (((Get-ADDomain | Select-Object PDCEmulator).PDCEmulator -replace "\..*$","")
     # get all users with PasswordNotRequired set
     $nonCompliantUsers = @()
     $nonCompliantUsers += Get-Aduser -filter {PasswordNotRequired -eq $true} -properties whenChanged
-    # remove the accounts tied to domain trust from the list
+    # remove the domain trust accounts
     foreach ($domain in $trustedDomains) {
         $nonCompliantUsers = $nonCompliantUsers | where-object {$_.name -ne "$($domain)$"}
     }
 
-    # return the list
+    # return the list if there are any
     if ($nonCompliantUsers) {
         return $nonCompliantUsers
     } else {
-        return $null
+        return 0
     }
 } else {
-    return $null
+    return 0
 }
